af565cf9d27d7cead7c295cd45c8dce9b8d3d2fb
Alfredo Holz Junior <alfredojrgasper@gmail.com>
2020-01-22 23:09:06 -0300

    animações com css
    Finalmente fiz alguma coisa um pouco além do html.... uma rotação em css hahahahha, bem, além disso alguns outros conceitos e novas bibliotecas.

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Router from './routes';
 
+import GlobalStyle from './styles/global';
+
 function App() {
-  return <Router />
+
+  return (
+    <>
+      <Router />
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
