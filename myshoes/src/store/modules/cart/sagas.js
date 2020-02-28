4fb18cb099176bdb9659f8b04e327b8020fcb825
Alfredo Holz Junior <alfredojrgasper@gmail.com>
2020-02-27 23:17:57 -0300

    finalizando o redux-saga

--- myshoes/src/store/modules/cart/sagas.js
+++ myshoes/src/store/modules/cart/sagas.js
@@ -1,15 +1,68 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
+import history from '../../../services/history';
+import {formatPrice} from '../../../util/format';
 
-import {addToCartSuccess} from './action';
+import {addToCartSuccess, updateAmountSuccess } from './action';
 
 function* addToCart({id}){
-  const response = yield call(api.get,`/products/${id}`);
 
-  yield put(addToCartSuccess(response.data));
+  const productExists = yield select(
+    state => state.cart.find(p=> p.id === id),
+  )
+
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+  
+  if(amount > stockAmount) {
+    toast.error('Estoque chegou ao limite!');
+    return;
+  }
+  
+
+  if(productExists){
+    const amount = productExists.amount + 1;
+
+    yield put(updateAmountSuccess(id, amount));
+
+  } else {
+
+      const response = yield call(api.get,`/products/${id}`);
+
+      const data = {
+        ...response.data,
+        amount: 1,
+        princeFormatted: formatPrice(response.data.price),
+      };
+
+      yield put(addToCartSuccess(data));
+      history.push('/cart');
+    }
+  }
+
+function* updateAmount({id, amount}){
+  if(amount <= 0) return;
+
+  const stock = yield call(api.get,`stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if(amount > stockAmount){
+    toast.error('Estoque chegou ao limite!');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+
+
 }
 
 export default all([
   takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
 ])
